a18f28d9670b20e9a41b4ae158c667c268e4ab4d
Federico Villagra <federico.villagra@mi.unc.edu.ar>
2024-08-15 15:07:24 -0300

    testing 1

--- src/config/config.js
+++ src/config/config.js
@@ -7,10 +7,10 @@ export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 // GitHub - JWT
 export const JWT_CLIENT_ID = process.env.JWT_CLIENT_ID;
 export const JWT_CLIENT_SECRET = process.env.JWT_CLIENT_SECRET;
+export const SIGNED_COOKIE_KEY = process.env.SIGNED_COOKIE_KEY;
 
 export const SECRET_PASS = process.env.SECRET_PASS;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const SIGNED_COOKIE_KEY = process.env.SIGNED_COOKIE_KEY;
 
 export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
